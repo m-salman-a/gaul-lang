9d370154c01f8f8e5e3a7c818822a276734816ea
m salman a <mohammadalfarisi6@gmail.com>
2022-10-14 09:42:25 +0700

    feat: refactor PeekableIterator

---
+++ src/peekableIterator.js
@@ -0,0 +1,24 @@
+export default class PeekableIterator {
+  constructor (string) {
+    this.it = string[Symbol.iterator]();
+    this.#peekNext();
+  }
+
+  #peekNext () {
+    this.peek = this.it.next();
+  }
+
+  [Symbol.iterator] () {
+    return this;
+  }
+
+  next () {
+    const { value, done } = this.peek;
+
+    if (done) { return { value: { current: undefined, next: undefined }, done: true }; }
+
+    this.#peekNext();
+
+    return { value: { current: value, next: this.peek.value }, done: false };
+  }
+}
--- tests/peekableIterator.test.js
+++ tests/peekableIterator.test.js
@@ -1,39 +1,39 @@
-import PeekableStream from "../src/peekablestream";
+import PeekableIterator from "../src/peekableIterator";
 
 test("WHEN next() is called THEN result should be first member & peek should be second member from the iterable.", () => {
 	// Arrange
-	const sut = new PeekableStream("Hello, world!");
+	const sut = new PeekableIterator("Hello, world!");
 
 	// Act
 	const result = sut.next();
 
 	// Assert
-	expect(result).toStrictEqual({ value: "H", done: false });
-	expect(sut.peek).toStrictEqual({ value: "e", done: false });
+	expect(result).toStrictEqual({
+		value: { current: "H", next: "e" },
+		done: false,
+	});
 });
 
 test("WHEN next() is called with empty string THEN result & peek should be undefined and done.", () => {
 	// Arrange
-	const sut = new PeekableStream("");
+	const sut = new PeekableIterator("");
 
 	// Act
 	const result = sut.next();
 
 	// Assert
-	expect(sut.next()).toStrictEqual({ value: undefined, done: true });
-	expect(sut.peek).toStrictEqual({ value: undefined, done: true });
+	expect(result).toStrictEqual({
+		value: { current: undefined, next: undefined },
+		done: true,
+	});
 });
 
 test("WHEN next() is called in a loop looped THEN should output the whole string as long as result isn't done.", () => {
-	const sut = new PeekableStream("Hello, World");
+	const sut = new PeekableIterator("Hello, World");
 	let str = "";
 
-	while (true) {
-		let result = sut.next();
-
-		if (result.done) break;
-
-		str += result.value;
+	for (var result of sut) {
+		str += result.current;
 	}
 
 	expect(str).toBe("Hello, World");
