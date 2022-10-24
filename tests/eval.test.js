2725369c566c60b2bcd44577b623b07030a105f3
m salman a <mohammadalfarisi6@gmail.com>
2022-10-24 18:55:20 +0700

    feat: separate eval tests from parser, add tests for program

---
+++ tests/eval.test.js
@@ -0,0 +1,74 @@
+import * as BinaryExpression from "../src/nodes/binary-expression";
+import * as Literal from "../src/nodes/literal";
+import * as Statement from "../src/nodes/statement";
+import * as UnaryExpression from "../src/nodes/unary-expression";
+import { Variable } from "../src/nodes/variable";
+import { Scope } from "../src/nodes/scope";
+
+test("WHEN given NumberLiteral SHOULD eval to number", () => {
+	const sut = new Literal.Number(10);
+
+	expect(sut.eval(10));
+});
+
+test("WHEN give StringLiteral SHOULD eval to a string", () => {
+	const sut = new Literal.String(`"10"`);
+
+	expect(sut.eval("10"));
+});
+
+test("WHEN given a Negative UnaryExpression SHOULD eval to a negative number", () => {
+	const sut = new UnaryExpression.Negative(new Literal.Number(10));
+
+	expect(sut.eval()).toBe(-10);
+});
+
+test("WHEN multiplying or dividing three numbers SHOULD go from left to right", () => {
+	const sut = new BinaryExpression.Divide(
+		new BinaryExpression.Multiply(
+			new Literal.Number("10"),
+			new Literal.Number("5")
+		),
+		new Literal.Number("8")
+	);
+
+	expect(sut.eval()).toBe(6.25);
+});
+
+test("WHEN adding or subtracting three numbers SHOULD go from left to right", () => {
+	const sut = new BinaryExpression.Subtract(
+		new BinaryExpression.Add(new Literal.Number("10"), new Literal.Number("5")),
+		new Literal.Number("8")
+	);
+
+	expect(sut.eval()).toBe(7);
+});
+
+test("WHEN given a tree with a complex expression SHOULD follow PEMDAS", () => {
+	const sut = new BinaryExpression.Subtract(
+		new BinaryExpression.Add(
+			new Literal.Number("10"),
+			new BinaryExpression.Multiply(
+				new Literal.Number("2"),
+				new BinaryExpression.Add(
+					new Literal.Number("5"),
+					new Literal.Number("8")
+				)
+			)
+		),
+		new Literal.Number("8")
+	);
+
+	expect(sut.eval()).toBe(28);
+});
+
+test("WHEN given an assignment SHOULD set variable on scope", () => {
+	const variable = new Variable("foo");
+	const scope = new Scope();
+	const sut = new Statement.Assignment(variable, new Literal.Number("10"));
+
+	sut.eval(scope);
+
+	expect(scope.get("foo")).toBe(10);
+	expect(variable.eval(scope)).toBe(10);
+});
--- tests/parser.test.js
+++ tests/parser.test.js
@@ -6,15 +6,13 @@ import { Variable } from "../src/nodes/variable";
 import { Program } from "../src/nodes/program";
 import Lexer from "../src/lexer";
 import Parser from "../src/parser";
-import { Scope } from "../src/nodes/scope";
 
 test("WHEN given number token SHOULD return a Number Literal", () => {
 	const sut = _setupSUT("10");
 
 	const ast = sut.parse();
 
-	expect(ast).toStrictEqual(new Program(new Literal.Number("10")));
-	expect(ast.eval()).toBe(10);
+	expect(ast).toStrictEqual(new Program([new Literal.Number("10")]));
 });
 
 test("WHEN given string token SHOULD return a String Literal", () => {
@@ -22,8 +20,7 @@ test("WHEN given string token SHOULD return a String Literal", () => {
 
 	const ast = sut.parse();
 
-	expect(ast).toStrictEqual(new Program(new Literal.String("10")));
-	expect(ast.eval()).toBe("10");
+	expect(ast).toStrictEqual(new Program([new Literal.String("10")]));
 });
 
 test("WHEN negative number token SHOULD return a Unary Expression and Number Literal", () => {
@@ -32,56 +29,53 @@ test("WHEN negative number token SHOULD return a Unary Expression and Number Lit
 	const ast = sut.parse();
 
 	expect(ast).toStrictEqual(
-		new Program(new UnaryExpression.Negative(new Literal.Number("10")))
+		new Program([new UnaryExpression.Negative(new Literal.Number("10"))])
 	);
-	expect(ast.eval()).toBe(-10);
 });
 
-test("WHEN multiplying three numbers SHOULD return nested Multiply Expression", () => {
+test("WHEN multiplying or dividing numbers SHOULD build tree going left to right", () => {
 	const sut = _setupSUT("10 * 5 / 8");
 
 	const ast = sut.parse();
 
 	expect(ast).toStrictEqual(
-		new Program(
+		new Program([
 			new BinaryExpression.Divide(
 				new BinaryExpression.Multiply(
 					new Literal.Number("10"),
 					new Literal.Number("5")
 				),
 				new Literal.Number("8")
-			)
-		)
+			),
+		])
 	);
-	expect(ast.eval()).toBe(6.25);
 });
 
-test("WHEN adding three numbers SHOULD return nested Add Expression", () => {
+test("WHEN adding or subtracting numbers SHOULD build tree going left to right", () => {
 	const sut = _setupSUT("10 + 5 - 8");
 
 	const ast = sut.parse();
 
 	expect(ast).toStrictEqual(
-		new Program(
+		new Program([
 			new BinaryExpression.Subtract(
 				new BinaryExpression.Add(
 					new Literal.Number("10"),
 					new Literal.Number("5")
 				),
 				new Literal.Number("8")
-			)
-		)
+			),
+		])
 	);
-	expect(ast.eval()).toBe(7);
 });
 
-test("WHEN given a complex expression SHOULD follow PEMDAS", () => {
+test("WHEN given a complex expression SHOULD build tree following follow PEMDAS", () => {
 	const sut = _setupSUT("10 + 2 * (5 + 8) - 8");
 
 	const ast = sut.parse();
 
 	expect(ast).toStrictEqual(
-		new Program(
+		new Program([
 			new BinaryExpression.Subtract(
 				new BinaryExpression.Add(
 					new Literal.Number("10"),
@@ -94,90 +88,99 @@ test("WHEN given a complex expression SHOULD follow PEMDAS", () => {
 					)
 				),
 				new Literal.Number("8")
-			)
-		)
+			),
+		])
 	);
-	expect(ast.eval()).toBe(28);
 });
 
-test("WHEN given an assignment statement SHOULD set variable in global scope", () => {
+test("WHEN given an assignment statement SHOULD return an Assignment Statement", () => {
 	const sut = _setupSUT("foo itu 10");
 
 	const ast = sut.parse();
-	const foo = new Variable("foo", ast.globalScope);
 
 	expect(ast).toStrictEqual(
-		new Program(
-			new Statement.Assignment(foo, new Literal.Number("10"), ast.globalScope)
-		)
+		new Program([
+			new Statement.Assignment(new Variable("foo"), new Literal.Number("10")),
+		])
 	);
-
-	ast.eval();
-
-	expect(ast.globalScope.get("foo")).toBe(10);
-	expect(foo.eval()).toBe(10);
 });
 
 test("WHEN given an assignment statement with string SHOULD set variable in global scope", () => {
 	const sut = _setupSUT(`foo itu "10"`);
 
 	const ast = sut.parse();
-	const foo = new Variable("foo", ast.globalScope);
 
 	expect(ast).toStrictEqual(
-		new Program(
-			new Statement.Assignment(foo, new Literal.String("10"), ast.globalScope)
-		)
+		new Program([
+			new Statement.Assignment(new Variable("foo"), new Literal.String("10")),
+		])
 	);
-
-	ast.eval();
-
-	expect(ast.globalScope.get("foo")).toBe("10");
-	expect(foo.eval()).toBe("10");
 });
 
 test("WHEN given a Boolean value SHOULD return Boolean", () => {
 	const sut = _setupSUT("foo itu benar");
 
 	const ast = sut.parse();
-	const foo = new Variable("foo", ast.globalScope);
 
 	expect(ast).toStrictEqual(
-		new Program(
-			new Statement.Assignment(foo, new Literal.Boolean(true), ast.globalScope)
-		)
+		new Program([
+			new Statement.Assignment(new Variable("foo"), new Literal.Boolean(true)),
+		])
 	);
-
-	ast.eval();
-
-	expect(ast.globalScope.get("foo")).toBe(true);
-	expect(foo.eval()).toBe(true);
 });
 
 test("WHEN given an if block SHOULD return correct If type", () => {
 	const sut = _setupSUT(`
 kalo benar
-	foo itu 5
+	foo itu 10
 	foo itu foo + 5
 yaudah
 `);
 
 	const ast = sut.parse();
-	const foo = new Variable("foo");
 
 	expect(ast).toStrictEqual(
-		new Statement.If(
-			new Literal.Boolean(true),
-			new Statement.Multiple([
-				new Statement.Assignment(foo, new Literal.Number("5"), sut.globalScope),
-				new Statement.Assignment(
-					foo,
-					new BinaryExpression.Add(foo, new Literal.Number("5")),
-					sut.globalScope
-				),
-			]),
-			new Statement.Multiple([])
-		)
+		new Program([
+			new Statement.If(
+				new Literal.Boolean(true),
+				new Statement.Multiple([
+					new Statement.Assignment(
+						new Variable("foo"),
+						new Literal.Number("10")
+					),
+					new Statement.Assignment(
+						new Variable("foo"),
+						new BinaryExpression.Add(
+							new Variable("foo"),
+							new Literal.Number("5")
+						)
+					),
+				]),
+				new Statement.Multiple([])
+			),
+		])
+	);
+});
+
+test("WHEN given multiple expressions SHOULD return a Block statement", () => {
+	const sut = _setupSUT(`
+foo itu 5
+foo itu 10 + 5
+`);
+
+	const ast = sut.parse();
+
+	expect(ast).toStrictEqual(
+		new Program([
+			new Statement.Assignment(new Variable("foo"), new Literal.Number("5")),
+			new Statement.Assignment(
+				new Variable("foo"),
+				new BinaryExpression.Add(
+					new Literal.Number("10"),
+					new Literal.Number("5")
+				)
+			),
+		])
 	);
 });
 
